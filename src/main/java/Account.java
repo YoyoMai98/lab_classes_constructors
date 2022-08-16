c81be1676720ef991e1da0fa754538ccec645f63
Yongran <maiyongran@gmail.com>
2022-08-16 14:16:11 +0100

    add properties of Account

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,6 +1,12 @@
 public class Account {
 
-    public Account(){
-        
+    private String id;
+    private String name;
+    private int balance;
+
+    public Account(String id, String name, int balance){
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
     }
 }
