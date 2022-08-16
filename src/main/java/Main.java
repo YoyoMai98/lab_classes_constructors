e9775e6d40c33cfbbd2c9a797b7e6e1e198c4e07
Yongran <maiyongran@gmail.com>
2022-08-16 14:25:13 +0100

    test Account getter methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,8 @@ public class Main {
         Account myAccount = new Account("1", "Johnson", 300);
 
         System.out.println(myAccount);
+
+        int myBalance = myAccount.getBalance();
+        System.out.println(myBalance);
     }
 }
