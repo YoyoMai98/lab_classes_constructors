3ab39ec0e3b47852fa3f8e2c8351b03765ca49df
Yongran <maiyongran@gmail.com>
2022-08-16 14:41:49 +0100

    add properties of Rectangle

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,4 +1,10 @@
 public class Rectangle {
 
-    public Rectangle(){}
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
 }
