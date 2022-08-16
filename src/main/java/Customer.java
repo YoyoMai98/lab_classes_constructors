aabf7576bfa8d52d82adb0f1bec0643b701962b9
Yongran <maiyongran@gmail.com>
2022-08-16 15:19:28 +0100

    add id property of Customer

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,10 +1,12 @@
 public class Customer {
 
+    private int id;
     private String firstName;
     private String lastName;
     private double heldMoney;
 
-    public Customer(String firstName, String lastName, double heldMoney){
+    public Customer(int id, String firstName, String lastName, double heldMoney){
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.heldMoney = heldMoney;
