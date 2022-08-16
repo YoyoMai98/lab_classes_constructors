a077d6326ce3661f544f8d981e5b29700c58ddfb
Yongran <maiyongran@gmail.com>
2022-08-16 14:43:20 +0100

    add getters methods in Rectangle

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -7,4 +7,12 @@ public class Rectangle {
         this.length = length;
         this.width = width;
     }
+
+    public double getLength(){
+        return this.length;
+    }
+
+    public double getWidth(){
+        return this.width;
+    }
 }
